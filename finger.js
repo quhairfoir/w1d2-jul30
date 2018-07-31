302a5bd7aa32c39d096de4516cb12782e883744e
Morag St. Clair <moragstc@gmail.com>
2018-07-31 14:29:37 +0000

    refactored finger.js tested and ready

--- finger.js
+++ finger.js
@@ -2,10 +2,18 @@
 //First LHL project
 
 //## Objective
-//Very cool little finger algorithm to multiply some numbers. 
+//Very cool little finger algorithm to multiply some numbers.
 
 //## How it works
 //1. Let's say I want to multiply two numbers (single digit) from 6-10
+function makeJoinedFingers(f) {
+  return f - 5;
+}
+
+function makeRoamingFingers(f) {
+  return 10 - f;
+}
+
 
 var left = process.argv[2];
 var right = process.argv[3];
@@ -20,26 +28,17 @@ console.log("Right: ", right);
 //  ring = 9
 //  pinky = 10
 
-var leftJoinedFingers = left - 5;
-var rightJoinedFingers = right - 5;
-
-var leftRoamingFingers = 10 - left;
-var rightRoamingFingers = 10 - right;
-
-console.log('Left hand: joined - roaming ', leftJoinedFingers, leftRoamingFingers);
-console.log('Right hand: joined - roaming ', rightJoinedFingers, rightRoamingFingers);
-
 //3. Join all the fingers that represent the 2 numbers being multiplied.
-var joinedFingers = leftJoinedFingers + rightJoinedFingers;
+
 //4. Then, count how many fingers are joined... x10.
-joinedFingers *= 10; 
+var joinedFingers = (makeJoinedFingers(left) + makeJoinedFingers(right)) * 10;
 
 console.log("Joined: ", joinedFingers);
-//5. Lastly, count roaming fingers from left hand and multiply by roaming fingers in right hand. 
-var roamingFingers = leftRoamingFingers * rightRoamingFingers;
+//5. Lastly, count roaming fingers from left hand and multiply by roaming fingers in right hand.
+var roamingFingers = makeRoamingFingers(left) * makeRoamingFingers(right);
 console.log("Roaming: ", roamingFingers);
 
-//6. Add step 4 and step 5. 
+//6. Add step 4 and step 5.
 var proof = joinedFingers + roamingFingers;
 
 console.log("Proof: ", proof);
